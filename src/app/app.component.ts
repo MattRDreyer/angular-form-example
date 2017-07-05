214b70ab20773da9391579ae29656d4d8020cbce
Matt Dreyer <matthew.dreyer@libertymutual.com>
2017-07-05 10:01:18 -0400

    forms submitting data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
+
+  onSubmit(data: NgForm){  //the word data here is inconsequential- could be anything
+  console.log(data.value)  //the word data here needs to match the above data word
+  }
+
 }
